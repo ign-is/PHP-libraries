ba4876516efc3cdc2fe6813a0c3a83986dd652b7
ign-is <igna.dev.contact@gmail.com>
2025-02-17 15:59:43 -0300

    first commit

---
+++ generar-pdf/pdf_para_generar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generar PDF</title>
+    <style type="text/css">
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        h1 {
+            background: red;
+        }
+        p {
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola Mundo desde una libreria de PHP para hacer PDFs</h1>
+    <p>Master en PHP</p>
+</body>
+</html>
